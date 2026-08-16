2961152ec1fd7ae0077a06fc59a93fe891f94cba
HeriCraft_HUB <setertop92222@gmail.com>
2026-08-16 22:58:55 +0300

    keys.lua

--- keys.lua
+++ keys.lua
@@ -1,3 +1,27 @@
+-- ============================================
+-- HERRICRAFT HUB - КЛЮЧИ И HWID
+-- ============================================
+
 return {
-    { key = "TEST-KEY-001", hwid = "11199431779_Enum.Platform.Windows" },
+    -- ===== 1 АКТИВАЦИЯ (ОДНОРАЗОВЫЙ) =====
+    { key = "KEY-001", max_activations = 1, hwids = {} },
+    { key = "KEY-002", max_activations = 1, hwids = {} },
+    { key = "KEY-003", max_activations = 1, hwids = {} },
+    
+    -- ===== 2 АКТИВАЦИИ =====
+    { key = "FREE-2", max_activations = 2, hwids = {} },
+    
+    -- ===== 5 АКТИВАЦИЙ =====
+    { key = "FREE-5", max_activations = 5, hwids = {} },
+    
+    -- ===== 10 АКТИВАЦИЙ =====
+    { key = "FREE-10", max_activations = 10, hwids = {} },
+    
+    -- ===== 100 АКТИВАЦИЙ (КОНКУРС) =====
+    { key = "CONTEST-2026", max_activations = 100, hwids = {} },
+    
+    -- ============================================
+    -- ДОБАВЛЯЙ НОВЫЕ КЛЮЧИ СЮДА:
+    -- ============================================
+    -- { key = "МОЙ_КЛЮЧ", max_activations = 3, hwids = {} },
 }
